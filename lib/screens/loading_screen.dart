9fc928a0d62e393cc5abdad8954ce9ecdd979f17
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2022-11-05 13:02:43 +0200

    4. Add http Package & Use it to make Network Request & Fetch Weather Data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import '../services/location.dart';
 
@@ -18,6 +19,7 @@ class LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getWeatherData();
     return const Scaffold();
   }
 
@@ -26,5 +28,17 @@ class LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     print(location.longitude);
     print(location.latitude);
+  }
+
+  void getWeatherData() async {
+    final response = await http.get(
+      Uri.parse(
+          'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=60d17fd4fec338a835ad9d77b1c6ddb4'),
+    );
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
   }
 }

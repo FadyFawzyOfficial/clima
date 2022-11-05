975b47f3ae24d1cd3408a04c639bcd95449888b8
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2022-11-05 15:58:40 +0200

    6. Refactor & Get Actual Weather Data from the OpenWeatherMap API based on Actual Location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 import '../services/location.dart';
+import '../services/networking.dart';
+
+const String apiKey = '60d17fd4fec338a835ad9d77b1c6ddb4';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -13,6 +13,9 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class LoadingScreenState extends State<LoadingScreen> {
+  late double latitude;
+  late double longitude;
+
   @override
   void initState() {
     super.initState();
@@ -21,33 +24,20 @@ class LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    getWeatherData();
     return const Scaffold();
   }
 
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.longitude);
-    print(location.latitude);
-  }
 
-  void getWeatherData() async {
-    final response = await http.get(
-      Uri.parse(
-          'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=60d17fd4fec338a835ad9d77b1c6ddb4'),
-    );
-    if (response.statusCode == 200) {
-      print(response.body);
-      final data = jsonDecode(response.body);
-      String cityName = data['name'];
-      double temp = data['main']['temp'];
-      int condition = data['weather'][0]['id'];
-      print(cityName);
-      print(temp);
-      print(condition);
-    } else {
-      print(response.statusCode);
-    }
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        url:
+            'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    final weatherData = await networkHelper.getData();
+    print(weatherData['name']);
   }
 }
---
+++ lib/services/networking.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class NetworkHelper {
+  final String url;
+
+  NetworkHelper({
+    required this.url,
+  });
+
+  Future getData() async {
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
